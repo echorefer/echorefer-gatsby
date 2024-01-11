96235bd719152f0762247c8cde57548496c90a60
Fabio Dessi <f.dessi@fabiodessi.dev>
2024-01-12 00:04:18 +0100

    feat: footer layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Paper } from '@mui/material';
 
 import Header from './Header/Header';
 import Main from './Main';
-import Footer from './Footer';
+import Footer from './Footer/Footer';
 
 import type { ReactNode } from 'react';
 
